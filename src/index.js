8ce40714d659aa2fc9c88e5e6011e0d5fc39ead0
Ayush Indoria <96787650+akkii03@users.noreply.github.com>
2022-09-26 00:12:57 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,9 +6,35 @@ const port = 8080
 // Parse JSON bodies (as sent by API clients)
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-const connection = require('./connector')
+const connection = require('./connector');
 
 
+app.get(`/api/orders`,async(req,res)=>{
+  const {limit,offset} = req.query;
+
+  if(!limit && !offset || !Number.isInteger(limit) || !Number.isInteger(offset)) {
+   const qry = `SELECT * FROM ORDERS LIMIT 10 OFFSET 0`;
+   connection.query(qry,(err,response)=>{
+      if(err) {
+         console.log("not response from db due to ",err);
+      }else{
+         res.status(200).json(response);
+      }
+   })
+  }
+
+  else{
+   const qry = `SELECT * FROM ORDERS LIMIT ${limit} OFFSET ${offset}`;
+   connection.query(qry,(err,response)=>{
+      if(err) {
+         console.log("no response from db due to ",err);
+
+      }else{
+         res.status(200).json(response);
+      }
+   })
+  }
+});
 
 app.listen(port, () => console.log(`App listening on port ${port}!`))
 
