dc5beb4e892a464e2ba81c557162ca8fb4559e4c
Arvind Sharma <102983268+parvindr091719@users.noreply.github.com>
2022-12-27 14:49:07 +0530

    Update createDatabase.js

--- src/createDatabase.js
+++ src/createDatabase.js
@@ -1,17 +1,39 @@
 
 let con = require('./connector')
-let data = require('./data');
+let data = require('./data')
+let a = async (err) => {
 
-con.query('CREATE TABLE orders(_id varchar(200),title varchar(100),description varchar(1000) )');
+    if (err) return console.log("failed to connect to mysql server/ database", err);
+    else {
+        await new Promise((resolve, reject) => {
+            con.query('DROP TABLE IF EXISTS orders', (err) => {
+                if (err) reject(err)
+                else {
+                    resolve(1)
+                }
+            })
+        })
+        await new Promise((resolve, reject) => {
+            con.query('CREATE TABLE orders(_id varchar(200),title varchar(100),description varchar(1000) )', (err) => {
+                if (err) reject(err)
+                else resolve(1)
+            })
+        })
+        for (let i = 0; i < data.length; i++)
+            await new Promise((resolve, reject) => {
+                con.query(`INSERT into orders values("${data[i]._id}","${data[i].title}","${data[i].description}")`, (err) => {
+                    if (err) reject(err)
+                    else { resolve(1) }
+                })
+            })
+    }
+    let [error, result] = await new Promise((resolve, reject) => {
+        con.query('SELECT * FROM orders', (err) => {
+            if (err) reject([err, undefined])
+            else resolve([undefined, data])
+        })
+    })
+    if (error) { console.log(er) }
 
-for(let i=0;i<data.length;i++) {
-    con.query(`INSERT into orders values("${data[i]._id}","${data[i].title}","${data[i].description}")`,(err,response)=>{
-        if(err) {
-            console.log("not insert due to ",err);
-        }else{
-            console.log("insertion successfully")
-        }
-    });
 }
-
-
+a()
